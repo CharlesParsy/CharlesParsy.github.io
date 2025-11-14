d5803d43e47a916b8bf0b867d4355c7022ce8613
CharlesParsy <102165510+CharlesParsy@users.noreply.github.com>
2025-11-14 08:44:48 +0100

    Add files via upload

---
+++ ARMwebsite/groups.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Groupes</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+<?php require_once("templates/header.html"); ?>
+
+<h1>Nos groupes</h1>
+
+<img class="piece" src="ARMgroup.png" alt="img ARM">
+
+<div class="bulle">
+    <p class="txt">L'ARM Haulchin permet à 10 groupes de répéter chaque semaine.</p>
+</div>
+
+<div class="red">
+    <div class="group">
+        <div>
+            <h2>Appetite</h2>
+            <p class="txt">Tribute Guns N'Roses</p>
+            <div>
+                <a class="bouton" href="https://www.facebook.com/AppetiteTributeGuns">Facebook</a>
+                <a class="bouton" href="https://www.youtube.com/channel/UCJ1j-w2isg6nna4q7pyScvQ">Youtube</a>
+            </div>
+            <br><br>
+        </div>
+        <img class="img-grp" src="groups/photo.png">
+    </div>
+
+    <div class="group">
+        <img class="img-grp" src="groups/photo (1).png">
+        <div>
+            <h2>Black Baccara</h2>
+            <p class="txt">Groupe de compositions Rock Alternatif</p>
+            <div>
+                <a class="bouton" href="https://www.facebook.com/profile.php?id=61561456162888">Facebook</a>
+                <a class="bouton" href="https://www.instagram.com/black_baccara_the_band/?igsh=ODZzYzdoeG41NnVl&utm_source=qr&fbclid=IwY2xjawNJyOxleHRuA2FlbQIxMABicmlkETBtMDA0bGZ6YnVxMHZ4ZkdlAR60fLGqRxUiFOvliMAFQbVAJmOa2q7k3gbebMhGTTsocbxhOR8a1gKwYAWSEg_aem_OuiOSVbQE4qLxdPIhcRpxQ#">Instagram</a>
+                <a class="bouton" href="https://www.youtube.com/@BlackBaccaraTheBand">Youtube</a>
+            </div>
+            <br><br>
+        </div>
+    </div>
+
+    <div class="group">
+        <div>
+            <h2>Converse Destroy</h2>
+            <p class="txt">Groupe de reprises punk</p>
+            <div>
+<!--                <a class="bouton" href="https://www.facebook.com/profile.php?id=100084248592332">Facebook</a>-->
+<!--                <a class="bouton" href="https://www.instagram.com/eyeblaze.band/">Instagram</a>-->
+            </div>
+            <br><br>
+        </div>
+        <img class="img-grp" src="groups/photo (7).png">
+    </div>
+
+    <div class="group">
+        <img class="img-grp" src="groups/photo (2).png">
+        <div>
+            <h2>DiRT</h2>
+            <p class="txt">Groupe de compositions Post Metal Prog Instrumental</p>
+            <div>
+                <a class="bouton" href="https://www.facebook.com/dirtrocktheband">Facebook</a>
+                <a class="bouton" href="https://www.instagram.com/dirtrockband/?fbclid=IwY2xjawNJxqRleHRuA2FlbQIxMABicmlkETBtMDA0bGZ6YnVxMHZ4ZkdlAR5Px9wGKNpn2gneP2i7G5l2nflihtZtCX9NNf47O6BOcTrRWullbZZESi3X1w_aem_l0H-k0LBnP_cUufjqnfbvA#">Instagram</a>
+                <a class="bouton" href="https://www.youtube.com/@dirtrockband">Youtube</a>
+            </div>
+            <br><br>
+        </div>
+    </div>
+
+    <div class="group">
+        <div>
+            <h2>Eyeblaze</h2>
+            <p class="txt">Groupe de compositions Rock</p>
+            <div>
+                <a class="bouton" href="https://www.facebook.com/profile.php?id=100084248592332">Facebook</a>
+                <a class="bouton" href="https://www.instagram.com/eyeblaze.band/">Instagram</a>
+            </div>
+            <br><br>
+        </div>
+        <img class="img-grp" src="groups/photo (3).png">
+    </div>
+
+    <div class="group">
+        <img class="img-grp" src="groups/photo (4).png">
+        <div>
+            <h2>Hard Life</h2>
+            <p class="txt">Groupe de reprises Rock</p>
+            <div>
+                <a class="bouton" href="https://www.facebook.com/groups/4324548697778880/">Facebook</a>
+            </div>
+            <br><br>
+        </div>
+    </div>
+
+    <div class="group">
+        <div>
+            <h2>Rewind</h2>
+            <p class="txt">Groupe de reprise Rock</p>
+            <div>
+                <a class="bouton" href="https://www.facebook.com/profile.php?id=61575622335799">Facebook</a>
+                <a class="bouton" href="https://www.instagram.com/rewind_coverband?fbclid=IwY2xjawNJzB1leHRuA2FlbQIxMABicmlkETBtMDA0bGZ6YnVxMHZ4ZkdlAR6sdHTwIHPotFc6EUisYDNnQVUcsIZ6ieu55j8EXIwu0rB4DFtDUWevANujnA_aem_5J_2kI_SaDtgnTCMxwfFNg">Instagram</a>
+            </div>
+            <br><br>
+        </div>
+        <img class="img-grp" src="groups/photo (5).png">
+    </div>
+
+    <div class="group">
+        <img class="img-grp" src="groups/photo (6).png">
+        <div>
+            <h2>Voice Of Rage</h2>
+            <p class="txt">Tribute Rage Against The Machine</p>
+            <div>
+                <a class="bouton" href="https://www.facebook.com/profile.php?id=100037191021444">Facebook</a>
+                <a class="bouton" href="https://www.youtube.com/@VoiceofRage">Youtube</a>
+            </div>
+            <br><br>
+        </div>
+    </div>
+</div>
+
+
+
+<?php require_once("templates/footer.html"); ?>
+
+</body>
+</html>
