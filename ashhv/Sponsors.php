9cc17646df006e5fd1be374ede5813a1a9e2d08d
CharlesParsy <102165510+CharlesParsy@users.noreply.github.com>
2026-03-02 12:19:48 +0100

    Add files via upload

---
+++ ashhv/Sponsors.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sponsors</title>
+    <link rel="shortcut icon" href="images/logo.jfif"/>
+    <link rel="stylesheet" href="style/style.css">
+    <script src="script/script.js" defer></script>
+    <!--    <script src="script/sponsors.js" defer></script>-->
+</head>
+
+<body>
+<?php require_once("templates/menu.html"); ?>
+
+<div id="main">
+    <br><br><br>
+
+    <h2>Nos Sponsors</h2>
+
+    <p>Ils nous suivent depuis l'année dernière</p>
+    <div id="sp1" class="sponsors">
+        <img class="sp" id="30" src="sponsors/sp30.png" alt="">
+        <img class="sp" id="31" src="sponsors/sp31.png" alt="">
+        <img class="sp" id="32" src="sponsors/sp32.png" alt="">
+        <img class="sp" id="18" src="sponsors/sp18.png" alt="">
+        <!--        <img class="sp" id="19" src="sponsors/sp19.png" alt="">-->
+        <img class="sp" id="20" src="sponsors/sp20.png" alt="">
+        <img class="sp" id="1" src="sponsors/sp1.png" alt="">
+        <img class="sp" id="16" src="sponsors/sp16.png" alt="">
+        <img class="sp" id="2" src="sponsors/sp2.png" alt="">
+        <img class="sp" id="22" src="sponsors/sp22.png" alt="">
+        <img class="sp" id="14" src="sponsors/sp14.png" alt="">
+        <img class="sp" id="4" src="sponsors/sp4.png" alt="">
+        <img class="sp" id="5" src="sponsors/sp5.png" alt="">
+        <img class="sp" id="24" src="sponsors/sp24.png" alt="">
+        <img class="sp" id="6" src="sponsors/sp6.png" alt="">
+        <!--        <img class="sp" id="13" src="sponsors/sp13.png" alt="">-->
+        <!--        <img class="sp" id="3" src="sponsors/sp3.png" alt="">-->
+        <!--        <img class="sp" id="7" src="sponsors/sp7.png" alt="">-->
+        <img class="sp" id="8" src="sponsors/sp8.png" alt="">
+        <!--        <img class="sp" id="9" src="sponsors/sp9.png" alt="">-->
+        <img class="sp" id="10" src="sponsors/sp10.png" alt="">
+        <!--        <img class="sp" id="11" src="sponsors/sp11.png" alt="">-->
+        <!--        <img class="sp" id="12" src="sponsors/sp12.png" alt="">-->
+        <!--        <img class="sp" id="15" src="sponsors/sp15.png" alt="">-->
+        <img class="sp" id="17" src="sponsors/sp17.png" alt="">
+        <img class="sp" id="33" src="sponsors/sp33.png" alt="">
+    </div>
+
+    <br><br>
+    <br><br>
+
+    <p>Ils nous rejoignent cette année</p>
+    <div id="sp2" class="sponsors">
+        <img class="sp" id="21" src="sponsors/sp21.png" alt="">
+        <img class="sp" id="23" src="sponsors/sp23.png" alt="">
+        <img class="sp" id="25" src="sponsors/sp25.png" alt="">
+        <img class="sp" id="26" src="sponsors/sp26.png" alt="">
+        <!--        <img class="sp" id="27" src="sponsors/sp27.png" alt="">-->
+        <img class="sp" id="29" src="sponsors/sp29.png" alt="">
+
+        <!--    <img class="sp" id="20" src="sp3.png" alt="" hidden>-->
+        <!--    <img class="sp" id="21" src="sp4.png" alt="" hidden>-->
+        <!--    <img class="sp" id="22" src="sp5.png" alt="" hidden>-->
+        <!--    <img class="sp" id="23" src="sp6.png" alt="" hidden>-->
+        <!--    <img class="sp" id="24" src="sp7.png" alt="" hidden>-->
+        <!--    <img class="sp" id="25" src="sp8.png" alt="" hidden>-->
+        <!--    <img class="sp" id="26" src="sp9.png" alt="" hidden>-->
+        <!--    <img class="sp" id="27" src="sp10.png" alt="" hidden>-->
+        <!--    <img class="sp" id="28" src="sp11.png" alt="" hidden>-->
+        <!--    <img class="sp" id="29" src="sp12.png" alt="" hidden>-->
+        <!--    <img class="sp" id="30" src="sp13.png" alt="" hidden>-->
+        <!--    <img class="sp" id="31" src="sp14.png" alt="" hidden>-->
+        <!--    <img class="sp" id="32" src="sp15.png" alt="" hidden>-->
+        <img class="sp" id="34" src="sponsors/sp34.png" alt="">
+        <img class="sp" id="35" src="sponsors/sp35.png" alt="">
+        <img class="sp" id="36" src="sponsors/sp36.png" alt="">
+        <img class="sp" id="37" src="sponsors/sp37.png" alt="">
+        <img class="sp" id="38" src="sponsors/sp38.png" alt="">
+    </div>
+
+    <br><br><br><br><br><br><br>
+
+    <?php require_once("templates/footer.html"); ?>
+</div>
+</body>
+</html>
---
+++ ashhv/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASHHV</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/logo.ico.jpg">
+    <link rel="icon" type="image/jpg" href="images/logo.jpg">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="script/script.js" defer></script>
+</head>
+
+<body>
+<?php require_once("templates/menu.html"); ?>
+<div id="main">
+    <br><br><br><br><br>
+    <img class="logo" src="images/logo.svg" alt="logo tournoi">
+    <br><br><br><br><br>
+    <div class="slogo"></div>
+
+    <br><br><br>
+    <h2>L'ASHHV fête ses 50 ans</h2>
+
+    <p class="justify">C'est en 1974 que Jean Paul Miroux, ancien élève de René Pruvot (membre du Rotary), paraplégique, joueur de basket en équipe de France Handisport, champion de France et médaillé Paralympique, a souhaité voir se créer à Valenciennes une association.
+        Le Rotary Club de Valenciennes répond à son souhait et crée L'Association Sportive des Handicapés du Hainaut Valenciennes.</p>
+    <br><br><br><br><br>
+
+<?php require_once("templates/footer.html"); ?>
+</div>
+</body>
+</html>
